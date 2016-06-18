777a9ada787fb943d2f9209fcf779d969fd9f413
Gyu-Ho Lee <gyuhox@gmail.com>
2016-06-18 13:07:15 -0700

    xlog: clean ups

---
+++ xlog/rotate/rotate_formatter.go
@@ -0,0 +1,54 @@
+package rotate
+
+import (
+	"bufio"
+	"strings"
+	"time"
+
+	"github.com/gyuho/distdb/xlog"
+)
+
+// Config contains configuration for log rotation.
+type Config struct {
+	Dir string
+
+	Debug bool
+}
+
+type formatter struct {
+	w     *bufio.Writer
+	debug bool
+}
+
+// NewFormatter returns a new formatter.
+func NewFormatter(cfg Config) xlog.Formatter {
+	return &formatter{}
+}
+
+func (rf *formatter) WriteFlush(pkg string, lvl xlog.LogLevel, txt string) {
+	if !rf.debug && lvl == xlog.DEBUG {
+		return
+	}
+
+	rf.w.WriteString(time.Now().String()[:26])
+	rf.w.WriteString(" " + lvl.String() + " | ")
+	if pkg != "" {
+		rf.w.WriteString(pkg + ": ")
+	}
+
+	rf.w.WriteString(txt)
+
+	if !strings.HasSuffix(txt, "\n") {
+		rf.w.WriteString("\n")
+	}
+
+	rf.w.Flush()
+}
+
+func (rf *formatter) SetDebug(debug bool) {
+	rf.debug = debug
+}
+
+func (rf *formatter) Flush() {
+	rf.w.Flush()
+}
