888c8749b1840d1f2c9060beae733e212522cf17
Gyu-Ho Lee <gyuhox@gmail.com>
2016-06-26 23:29:03 -0700

    raft: separate raft storage files

--- raft/raft_storage_in_memory.go
+++ raft/raft_storage_in_memory.go
@@ -6,48 +6,6 @@ import (
 	"github.com/gyuho/db/raft/raftpb"
 )
 
-// Storage defines storage interface that retrieves log entries from storage.
-//
-// (etcd raft.Storage)
-type Storage interface {
-	// GetState returns the saved HardState and ConfigState.
-	//
-	// (etcd raft.Storage.InitialState)
-	GetState() (raftpb.HardState, *raftpb.ConfigState, error)
-
-	// Term returns the term of the entry index, which must be in the range
-	// [FirstIndex - 1, LastIndex].
-	//
-	// The term of the entry before FirstIndex is retained for matching purposes,
-	// even if the rest of the entries in the term may not be available.
-	//
-	// (etcd raft.Storage.Term)
-	Term(idx uint64) (uint64, error)
-
-	// FirstIndex returns the index of the first-available log entry.
-	// Older entries have been incorporated into the latest snapshot.
-	//
-	// (etcd raft.Storage.FirstIndex)
-	FirstIndex() (uint64, error)
-
-	// LastIndex returns the index of the last log entry in storage.
-	//
-	// (etcd raft.Storage.LastIndex)
-	LastIndex() (uint64, error)
-
-	// Entries returns the slice of log entries in [startIdx, endIdx).
-	// limitSize limits the total size of log entries to return.
-	// It returns at least one entry if any.
-	//
-	// (etcd raft.Storage.Entries)
-	Entries(startIdx, endIdx, limitSize uint64) ([]raftpb.Entry, error)
-
-	// Snapshot returns the most recent snapshot.
-	// If the snapshot is temporarily unavailable, the state machine
-	// knows that Storage needs more time to prepare snapshot.
-	Snapshot() (raftpb.Snapshot, error)
-}
-
 // StorageInMemory implements Storage interface backed by in-memory storage.
 type StorageInMemory struct {
 	mu sync.Mutex
