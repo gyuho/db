d293e7aefcca3bf6c282298b7d3f00c0c9cf4297
Gyu-Ho Lee <gyuhox@gmail.com>
2016-06-26 23:40:23 -0700

    raft: add Entries test for 'StorageInMemory'

--- raft/raft_storage_in_memory_test.go
+++ raft/raft_storage_in_memory_test.go
@@ -1,6 +1,8 @@
 package raft
 
 import (
+	"math"
+	"reflect"
 	"testing"
 
 	"github.com/gyuho/db/raft/raftpb"
@@ -32,6 +34,117 @@ func Test_StorageInMemory_Term(t *testing.T) {
 		}
 		if term != tt.wterm {
 			t.Fatalf("#%d: term expected %d, got %d", i, tt.wterm, term)
+		}
+	}
+}
+
+func Test_StorageInMemory_Entries(t *testing.T) {
+	ents := []raftpb.Entry{
+		{Index: 3, Term: 3},
+		{Index: 4, Term: 4},
+		{Index: 5, Term: 5},
+		{Index: 6, Term: 6},
+	}
+
+	tests := []struct {
+		// Entries returns [startIdx, endIdx)
+		startIdx, endIdx, limitSize uint64
+
+		werr     error
+		wentries []raftpb.Entry
+	}{
+		{
+			2,
+			6,
+			math.MaxUint64,
+			ErrCompacted,
+			nil,
+		},
+
+		{
+			3,
+			4,
+			math.MaxUint64,
+			ErrCompacted,
+			nil,
+		},
+
+		{
+			4,
+			5,
+			math.MaxUint64,
+			nil,
+			[]raftpb.Entry{{Index: 4, Term: 4}},
+		},
+
+		{
+			4,
+			6,
+			math.MaxUint64,
+			nil,
+			[]raftpb.Entry{{Index: 4, Term: 4}, {Index: 5, Term: 5}},
+		},
+
+		{
+			4,
+			7,
+			math.MaxUint64,
+			nil,
+			[]raftpb.Entry{{Index: 4, Term: 4}, {Index: 5, Term: 5}, {Index: 6, Term: 6}},
+		},
+
+		// even limitSize 0 should return at least one entry
+		{
+			4,
+			7,
+			0,
+			nil,
+			[]raftpb.Entry{{Index: 4, Term: 4}},
+		},
+
+		// limit to 2 entries
+		{
+			4,
+			7,
+			uint64(ents[1].Size() + ents[2].Size()),
+			nil,
+			[]raftpb.Entry{{Index: 4, Term: 4}, {Index: 5, Term: 5}},
+		},
+
+		// limit to 2 entries
+		{
+			4,
+			7,
+			uint64(ents[1].Size() + ents[2].Size() + ents[3].Size()/2),
+			nil,
+			[]raftpb.Entry{{Index: 4, Term: 4}, {Index: 5, Term: 5}},
+		}, // limit to 2 entries
+
+		{
+			4,
+			7,
+			uint64(ents[1].Size() + ents[2].Size() + ents[3].Size() - 1),
+			nil,
+			[]raftpb.Entry{{Index: 4, Term: 4}, {Index: 5, Term: 5}},
+		},
+
+		{
+			4,
+			7,
+			uint64(ents[1].Size() + ents[2].Size() + ents[3].Size()),
+			nil,
+			[]raftpb.Entry{{Index: 4, Term: 4}, {Index: 5, Term: 5}, {Index: 6, Term: 6}},
+		},
+	}
+
+	for i, tt := range tests {
+		st := &StorageInMemory{snapshotEntries: ents}
+		entries, err := st.Entries(tt.startIdx, tt.endIdx, tt.limitSize)
+		if err != tt.werr {
+			t.Fatalf("#%d: error expected %v, got %v", i, tt.werr, err)
+		}
+		if !reflect.DeepEqual(entries, tt.wentries) {
+			t.Fatalf("#%d: entries expected %+v, got %+v", i, tt.wentries, entries)
 		}
 	}
 }
