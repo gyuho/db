ce49a74ded0ca3158a8e49b85b86a93eba03b16a
Gyu-Ho Lee <gyuhox@gmail.com>
2016-07-05 03:49:23 -0700

    raft: update error string, indentation in proto

--- raft/errors.go
+++ raft/errors.go
@@ -4,21 +4,21 @@ import "errors"
 
 var (
 	// ErrStopped is returned when a Node has been stopped.
-	ErrStopped = errors.New("stoppped")
+	ErrStopped = errors.New("raft: stoppped")
 
 	// ErrUnavailable is returned when creating snapshot and
 	// the requested log entries aren't available.
-	ErrUnavailable = errors.New("requested entry at index is unavailable")
+	ErrUnavailable = errors.New("raft: requested entry at index is unavailable")
 
 	// ErrCompacted indicates that requested index is unavailable
 	// because it predates the last snapshot.
-	ErrCompacted = errors.New("requested index is unavailable (already compacted)")
+	ErrCompacted = errors.New("raft: requested index is unavailable (already compacted)")
 
 	// ErrSnapOutOfDate is returned when creating snapshot and the requested
 	// index is older than the existing snapshot.
-	ErrSnapOutOfDate = errors.New("requested index is older than the existing snapshot")
+	ErrSnapOutOfDate = errors.New("raft: requested index is older than the existing snapshot")
 
 	// ErrSnapshotTemporarilyUnavailable is returned when the required
 	// snapshot is temporarily unavailable.
-	ErrSnapshotTemporarilyUnavailable = errors.New("snapshot is temporarily available")
+	ErrSnapshotTemporarilyUnavailable = errors.New("raft: snapshot is temporarily available")
 )
