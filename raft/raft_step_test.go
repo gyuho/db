7b2c859aa5c975dfc27a5df0a93f568e5f1366c2
Gyu-Ho Lee <gyuhox@gmail.com>
2016-07-14 01:20:10 -0700

    raft: add 'Test_Step_log_replication'

--- raft/raft_step_test.go
+++ raft/raft_step_test.go
@@ -1,6 +1,7 @@
 package raft
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/gyuho/db/raft/raftpb"
@@ -98,6 +99,17 @@ func Test_Step_log_replication(t *testing.T) {
 
 			2,
 		},
+
+		{
+			newFakeNetwork(nil, nil, nil),
+			[]raftpb.Message{
+				{Type: raftpb.MESSAGE_TYPE_PROPOSAL_TO_LEADER, From: 1, To: 1, Entries: []raftpb.Entry{{Data: []byte("testdata")}}},
+				{Type: raftpb.MESSAGE_TYPE_INTERNAL_TRIGGER_CAMPAIGN, From: 1, To: 2},
+				{Type: raftpb.MESSAGE_TYPE_PROPOSAL_TO_LEADER, From: 1, To: 2, Entries: []raftpb.Entry{{Data: []byte("testdata")}}},
+			},
+
+			4,
+		},
 	}
 
 	for i, tt := range tests {
@@ -113,10 +125,32 @@ func Test_Step_log_replication(t *testing.T) {
 		}
 
 		for id, machine := range tt.fakeNetwork.allStateMachines {
-			sm := machine.(*raftNode)
+			rnd := machine.(*raftNode)
 
-			if sm.storageRaftLog.committedIndex != tt.wCommittedIndex {
-				t.Fatalf("#%d: id %x committed index expected %d, got %d", i, id, tt.wCommittedIndex, sm.storageRaftLog.committedIndex)
+			if rnd.storageRaftLog.committedIndex != tt.wCommittedIndex {
+				t.Fatalf("#%d: id %x committed index expected %d, got %d", i, id, tt.wCommittedIndex, rnd.storageRaftLog.committedIndex)
+			}
+
+			var appliedEntries []raftpb.Entry
+			for _, ent := range persistALlUnstableAndApplyNextEntries(rnd, tt.fakeNetwork.allStableStorageInMemory[id]) {
+				if ent.Data != nil {
+					appliedEntries = append(appliedEntries, ent)
+				}
+			}
+
+			var proposedEntries []raftpb.Message
+			for _, m := range tt.msgsToSendOneByOne {
+				if m.Type == raftpb.MESSAGE_TYPE_PROPOSAL_TO_LEADER {
+					proposedEntries = append(proposedEntries, m)
+				}
+			}
+
+			for k := range proposedEntries {
+				ae := appliedEntries[k]
+				pe := proposedEntries[k]
+				if !bytes.Equal(ae.Data, pe.Entries[0].Data) {
+					t.Fatalf("#%d.%d: id %x entry data expected %q, got %q", i, k, id, pe.Entries[0].Data, ae.Data)
+				}
 			}
 		}
 	}
