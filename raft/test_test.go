7b3be7fcbbeba0ae4da6fc362c0eca3a87031844
Gyu-Ho Lee <gyuhox@gmail.com>
2016-07-17 14:43:04 -0700

    raft: move test 'allNodeIDs'

--- raft/test_test.go
+++ raft/test_test.go
@@ -3,6 +3,7 @@ package raft
 import (
 	"fmt"
 	"math/rand"
+	"reflect"
 	"testing"
 
 	"github.com/gyuho/db/raft/raftpb"
@@ -246,5 +247,28 @@ func Test_generateIDs(t *testing.T) {
 		}
 
 		id = prevID
+	}
+}
+
+// (etcd raft.TestRaftNodes)
+func Test_raft_allNodeIDs(t *testing.T) {
+	tests := []struct {
+		ids  []uint64
+		wids []uint64
+	}{
+		{
+			[]uint64{1, 2, 3},
+			[]uint64{1, 2, 3},
+		},
+		{
+			[]uint64{3, 2, 1},
+			[]uint64{1, 2, 3},
+		},
+	}
+	for i, tt := range tests {
+		rnd := newTestRaftNode(1, tt.ids, 10, 1, NewStorageStableInMemory())
+		if !reflect.DeepEqual(rnd.allNodeIDs(), tt.wids) {
+			t.Fatalf("#%d: all node IDs = %+v, want %+v", i, rnd.allNodeIDs(), tt.wids)
+		}
 	}
 }
