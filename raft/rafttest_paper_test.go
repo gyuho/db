65904f35ac459f9c5c44d36c806218348da15f64
Gyu-Ho Lee <gyuhox@gmail.com>
2016-07-22 06:40:09 -0700

    raft: add 'TestFollowerUpdateTermFromMessage'

--- raft/rafttest_paper_test.go
+++ raft/rafttest_paper_test.go
@@ -1,6 +1,30 @@
 package raft
 
+import (
+	"testing"
+
+	"github.com/gyuho/db/raft/raftpb"
+)
+
 // (etcd raft.TestFollowerUpdateTermFromMessage)
+func Test_raft_paper_follower_update_term_from_Message(t *testing.T) {
+	rnd := newTestRaftNode(1, []uint64{1, 2, 3}, 10, 1, NewStorageStableInMemory())
+	rnd.becomeFollower(1, 2) // leaderID 2
+
+	// case msg.SenderCurrentTerm > rnd.term:
+	//
+	// func (rnd *raftNode) sendToMailbox(msg raftpb.Message) {
+	//   msg.From = rnd.id
+	//
+	rnd.Step(raftpb.Message{Type: raftpb.MESSAGE_TYPE_LEADER_APPEND, SenderCurrentTerm: 2})
+
+	if rnd.term != 2 {
+		t.Fatalf("term expected 2, got %d", rnd.term)
+	}
+	if rnd.state != raftpb.NODE_STATE_FOLLOWER {
+		t.Fatalf("node state expected %q, got %q", raftpb.NODE_STATE_FOLLOWER, rnd.state)
+	}
+}
 
 // (etcd raft.TestCandidateUpdateTermFromMessage)
 
