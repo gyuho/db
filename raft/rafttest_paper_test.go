372008ba8b703fc1b0db890b4adbad3811d2419e
Gyu-Ho Lee <gyuhox@gmail.com>
2016-07-22 23:14:39 -0700

    raft: add 'paper_candidate_start_election'

--- raft/rafttest_paper_test.go
+++ raft/rafttest_paper_test.go
@@ -130,15 +130,83 @@ func Test_raft_paper_leader_broadcast_heartbeat(t *testing.T) {
 		{Type: raftpb.MESSAGE_TYPE_LEADER_HEARTBEAT, From: 1, To: 2, SenderCurrentTerm: 1},
 		{Type: raftpb.MESSAGE_TYPE_LEADER_HEARTBEAT, From: 1, To: 3, SenderCurrentTerm: 1},
 	}
-
 	if !reflect.DeepEqual(msgs, wmsgs) {
 		t.Fatalf("messages expected %+v, got %+v", wmsgs, msgs)
 	}
 }
 
+// (Raft §3.4 Leader election, p.16)
 // (etcd raft.TestFollowerStartElection)
+func Test_raft_paper_follower_start_election(t *testing.T) {
+	rnd := newTestRaftNode(1, []uint64{1, 2, 3}, 10, 1, NewStorageStableInMemory())
 
+	rnd.becomeFollower(1, 2) // term 1, leader is 2
+	oldTerm := rnd.term
+
+	// election timeout to trigger election from follower
+	for i := 0; i < 2*rnd.electionTimeoutTickNum; i++ {
+		rnd.tickFunc()
+	}
+
+	if rnd.term != oldTerm+1 {
+		t.Fatalf("term should have increased to %d, got %d", oldTerm+1, rnd.term)
+	}
+
+	rnd.assertNodeState(raftpb.NODE_STATE_CANDIDATE)
+
+	if !rnd.votedFrom[rnd.id] {
+		t.Fatalf("should have voted for itself, got %+v", rnd.votedFrom)
+	}
+
+	msgs := rnd.readAndClearMailbox()
+	sort.Sort(messageSlice(msgs))
+
+	// follower is now candidate sending vote-requests
+	wmsgs := []raftpb.Message{
+		{Type: raftpb.MESSAGE_TYPE_CANDIDATE_REQUEST_VOTE, From: 1, To: 2, SenderCurrentTerm: 2},
+		{Type: raftpb.MESSAGE_TYPE_CANDIDATE_REQUEST_VOTE, From: 1, To: 3, SenderCurrentTerm: 2},
+	}
+	if !reflect.DeepEqual(msgs, wmsgs) {
+		t.Fatalf("messages expected %+v, got %+v", wmsgs, msgs)
+	}
+}
+
+// (Raft §3.4 Leader election, p.16)
 // (etcd raft.TestCandidateStartNewElection)
+func Test_raft_paper_candidate_start_election(t *testing.T) {
+	rnd := newTestRaftNode(1, []uint64{1, 2, 3}, 10, 1, NewStorageStableInMemory())
+
+	rnd.becomeCandidate() // term 2
+	oldTerm := rnd.term
+
+	// election timeout to trigger another election from candidate
+	// candidate starts a new election and increments its term
+	for i := 0; i < 2*rnd.electionTimeoutTickNum; i++ {
+		rnd.tickFunc()
+	}
+
+	if rnd.term != oldTerm+1 {
+		t.Fatalf("term should have increased to %d, got %d", oldTerm+1, rnd.term)
+	}
+
+	rnd.assertNodeState(raftpb.NODE_STATE_CANDIDATE)
+
+	if !rnd.votedFrom[rnd.id] {
+		t.Fatalf("should have voted for itself, got %+v", rnd.votedFrom)
+	}
+
+	msgs := rnd.readAndClearMailbox()
+	sort.Sort(messageSlice(msgs))
+
+	// follower is now candidate sending vote-requests
+	wmsgs := []raftpb.Message{
+		{Type: raftpb.MESSAGE_TYPE_CANDIDATE_REQUEST_VOTE, From: 1, To: 2, SenderCurrentTerm: 2},
+		{Type: raftpb.MESSAGE_TYPE_CANDIDATE_REQUEST_VOTE, From: 1, To: 3, SenderCurrentTerm: 2},
+	}
+	if !reflect.DeepEqual(msgs, wmsgs) {
+		t.Fatalf("messages expected %+v, got %+v", wmsgs, msgs)
+	}
+}
 
 // (etcd raft.TestLeaderElectionInOneRoundRPC)
 
