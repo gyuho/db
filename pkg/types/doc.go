23b73a698bc9f2bf482f141f97c3a2a17095bfdf
Gyu-Ho Lee <gyuhox@gmail.com>
2016-07-24 22:25:12 -0700

    pkg/types: initial commit

---
+++ pkg/types/doc.go
@@ -0,0 +1,2 @@
+// Package types defines types.
+package types
---
+++ pkg/types/id_test.go
@@ -0,0 +1,82 @@
+package types
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
+
+func TestIDString(t *testing.T) {
+	tests := []struct {
+		input ID
+		want  string
+	}{
+		{
+			input: 12,
+			want:  "c",
+		},
+		{
+			input: 4918257920282737594,
+			want:  "444129853c343bba",
+		},
+	}
+
+	for i, tt := range tests {
+		got := tt.input.String()
+		if tt.want != got {
+			t.Fatalf("#%d: ID.String expected %v, got %v", i, tt.want, got)
+		}
+	}
+}
+
+func TestIDFromString(t *testing.T) {
+	tests := []struct {
+		input string
+		want  ID
+	}{
+		{
+			input: "17",
+			want:  23,
+		},
+		{
+			input: "612840dae127353",
+			want:  437557308098245459,
+		},
+	}
+
+	for i, tt := range tests {
+		got, err := IDFromString(tt.input)
+		if err != nil {
+			t.Fatalf("#%d: IDFromString %v", i, err)
+			continue
+		}
+		if tt.want != got {
+			t.Fatalf("#%d: IDFromString expected %v, got %v", i, tt.want, got)
+		}
+	}
+}
+
+func TestIDFromStringFail(t *testing.T) {
+	tests := []string{
+		"",
+		"XXX",
+		"612840dae127353612840dae127353",
+	}
+
+	for i, tt := range tests {
+		_, err := IDFromString(tt)
+		if err == nil {
+			t.Fatalf("#%d: IDFromString expected error, got err=nil", i)
+		}
+	}
+}
+
+func TestIDSlice(t *testing.T) {
+	g := []ID{10, 500, 5, 1, 100, 25}
+	w := []ID{1, 5, 10, 25, 100, 500}
+	sort.Sort(IDSlice(g))
+
+	if !reflect.DeepEqual(g, w) {
+		t.Fatalf("slice expected %v, got %v", w, g)
+	}
+}
