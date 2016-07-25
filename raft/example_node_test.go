d3b7dcf4bf7d14eee8e2ff1ee7e125c104ce531b
Gyu-Ho Lee <gyuhox@gmail.com>
2016-07-24 21:16:49 -0700

    raft: add 'example_node_test.go'

---
+++ raft/example_node_test.go
@@ -0,0 +1,28 @@
+package raft
+
+import "github.com/gyuho/db/raft/raftpb"
+
+func applyToStore(ents []raftpb.Entry)    {}
+func sendMessages(msgs []raftpb.Message)  {}
+func saveStateToDisk(st raftpb.HardState) {}
+func saveToDisk(ents []raftpb.Entry)      {}
+
+func ExampleNode() {
+	config := &Config{}
+	nd := StartNode(config, nil)
+	defer nd.Stop()
+
+	// last known hardState
+	var prevHardState raftpb.HardState
+	for {
+		rd := <-nd.Ready()
+		if !rd.HardStateToSave.Equal(prevHardState) {
+			saveStateToDisk(rd.HardStateToSave)
+			prevHardState = rd.HardStateToSave
+		}
+
+		saveToDisk(rd.EntriesToAppend)
+		go applyToStore(rd.EntriesToCommit)
+		sendMessages(rd.MessagesToSend)
+	}
+}
