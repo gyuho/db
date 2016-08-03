c4d66fbd7d5fa468d44de3b7c7edce5e897f9b5a
Gyu-Ho Lee <gyuhox@gmail.com>
2016-08-02 17:08:29 -0700

    pkg/netutil: add 'NewListener'

--- pkg/netutil/listener.go
+++ pkg/netutil/listener.go
@@ -1,6 +1,8 @@
 package netutil
 
 import (
+	"crypto/tls"
+	"fmt"
 	"net"
 	"os"
 )
@@ -30,7 +32,8 @@ func (lu *listenerUnix) Close() error {
 	return lu.Listener.Close()
 }
 
-// NewListenerUnix returns new net.Listener with unix socket.
+// NewListenerUnix returns new net.Listener with unix socket
+// (unix sockets via unix://address).
 //
 // (etcd pkg.transport.NewUnixListener)
 func NewListenerUnix(addr string) (net.Listener, error) {
@@ -42,4 +45,33 @@ func NewListenerUnix(addr string) (net.Listener, error) {
 		return nil, err
 	}
 	return &listenerUnix{l}, nil
+}
+
+// NewListener returns new net.Listener based on the scheme and tls.Config.
+//
+// (etcd pkg.transport.NewListener)
+func NewListener(addr, scheme string, tlsConfig *tls.Config) (l net.Listener, err error) {
+	switch scheme {
+	case "unix", "unixs":
+		l, err = NewListenerUnix(addr)
+		if err != nil {
+			return
+		}
+	case "http", "https":
+		l, err = net.Listen("tcp", addr)
+		if err != nil {
+			return
+		}
+	default:
+		return nil, fmt.Errorf("%q is not supported", scheme)
+	}
+
+	if scheme != "https" && scheme != "unixs" { // no need TLS
+		return
+	}
+	if tlsConfig == nil { // need TLS, but empty config
+		return nil, fmt.Errorf("cannot listen on TLS for %s: KeyFile and CertFile are not presented", scheme+"://"+addr)
+	}
+
+	return tls.NewListener(l, tlsConfig), nil
 }
