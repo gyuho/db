77d800e29e310f7c70d19445c63037e0f0d65827
Gyu-Ho Lee <gyuhox@gmail.com>
2016-08-03 12:06:07 -0700

    pkg/netutil: add 'Test_NewTransportTimeout'

--- pkg/netutil/transport_test.go
+++ pkg/netutil/transport_test.go
@@ -1,7 +1,11 @@
 package netutil
 
 import (
+	"bytes"
 	"crypto/tls"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 	"time"
@@ -44,5 +48,65 @@ func Test_NewTransport_TLSInfo(t *testing.T) {
 		if trans.TLSClientConfig == nil {
 			t.Fatalf("#%d: want non-nil TLSClientConfig", i)
 		}
+	}
+}
+
+// (etcd pkg.transport.TestNewTimeoutTransport)
+func Test_NewTransportTimeout(t *testing.T) {
+	tr, err := NewTransportTimeout(tlsutil.TLSInfo{}, time.Hour, time.Hour, time.Hour)
+	if err != nil {
+		t.Fatalf("unexpected NewTransportTimeout error: %v", err)
+	}
+
+	remoteAddr := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(r.RemoteAddr))
+	}
+	srv := httptest.NewServer(http.HandlerFunc(remoteAddr))
+
+	defer srv.Close()
+	conn, err := tr.Dial("tcp", srv.Listener.Addr().String())
+	if err != nil {
+		t.Fatalf("unexpected dial error: %v", err)
+	}
+	defer conn.Close()
+
+	tconn, ok := conn.(*connTimeout)
+	if !ok {
+		t.Fatalf("failed to dial out *connTimeout")
+	}
+	if tconn.readTimeout != time.Hour {
+		t.Errorf("read timeout = %s, want %s", tconn.readTimeout, time.Hour)
+	}
+	if tconn.writeTimeout != time.Hour {
+		t.Errorf("write timeout = %s, want %s", tconn.writeTimeout, time.Hour)
+	}
+
+	// ensure not reuse timeout connection
+	req, err := http.NewRequest("GET", srv.URL, nil)
+	if err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+	resp, err := tr.RoundTrip(req)
+	if err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+	addr0, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+
+	resp, err = tr.RoundTrip(req)
+	if err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+	addr1, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		t.Fatalf("unexpected err %v", err)
+	}
+
+	if bytes.Equal(addr0, addr1) {
+		t.Errorf("addr0 = %s addr1= %s, want not equal", string(addr0), string(addr1))
 	}
 }
