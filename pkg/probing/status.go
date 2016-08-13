de6796dfbbb5c22d734b4cef315dfc86cadb5a14
Gyu-Ho Lee <gyuhox@gmail.com>
2016-08-12 23:56:21 -0700

    pkg/probing: add status

--- pkg/probing/status.go
+++ pkg/probing/status.go
@@ -40,7 +40,7 @@ type status struct {
 // SRTT <- (1 - alpha) * SRTT + alpha * RTT
 const α = 0.125
 
-func (s *status) record(rtt time.Duration, when time.Time) {
+func (s *status) record(rtt time.Duration, requested time.Time) {
 	s.mu.Lock()
 
 	s.total++
@@ -49,7 +49,74 @@ func (s *status) record(rtt time.Duration, when time.Time) {
 	s.err = nil
 
 	s.srtt = time.Duration((1-α)*float64(s.srtt) + α*float64(rtt))
-	s.clockDiff = time.Now().Sub(when) - s.srtt/2
+	s.clockDiff = time.Now().Sub(requested) - s.srtt/2
 
 	s.mu.Unlock()
+}
+
+func (s *status) recordFailure(err error) {
+	s.mu.Lock()
+
+	s.total++
+	s.loss++
+
+	s.health = false
+	s.err = err
+
+	s.mu.Unlock()
+}
+
+func (s *status) reset() {
+	s.mu.Lock()
+
+	s.total = 0
+	s.loss = 0
+
+	s.health = false
+	s.err = nil
+
+	s.srtt = 0
+	s.clockDiff = 0
+
+	s.mu.Unlock()
+}
+
+func (s *status) Total() int64 {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.total
+}
+
+func (s *status) Loss() int64 {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.loss
+}
+
+func (s *status) Health() bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.health
+}
+
+func (s *status) Err() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.err
+}
+
+func (s *status) SRTT() time.Duration {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.srtt
+}
+
+func (s *status) ClockDiff() time.Duration {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.clockDiff
+}
+
+func (s *status) StopNotify() <-chan struct{} {
+	return s.stopc
 }
