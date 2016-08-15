4ca6429012d1d5dd2e5e9528d0c1e3784f5baa95
Gyu-Ho Lee <gyuhox@gmail.com>
2016-08-14 17:40:25 -0700

    rafthttp: adding streamReader tests

--- rafthttp/12_stream_reader_test.go
+++ rafthttp/12_stream_reader_test.go
@@ -1,1 +1,18 @@
 package rafthttp
+
+import "testing"
+
+// (etcd rafthttp.TestStreamReaderDialRequest)
+func Test_streamReader_dial_request(t *testing.T) {
+
+}
+
+// (etcd rafthttp.TestStreamReaderDialResult)
+func Test_streamReader_dial_result(t *testing.T) {
+
+}
+
+// (etcd rafthttp.TestStream)
+func Test_streamReader(t *testing.T) {
+
+}
