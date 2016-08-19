82da20372bcf5dbdfa1eb83b113e0c19f24e637c
Gyu-Ho Lee <gyuhox@gmail.com>
2016-08-19 02:09:55 -0700

    raft-example: add store implementation

--- raft-example/store_test.go
+++ raft-example/store_test.go
@@ -3,17 +3,27 @@ package main
 import (
 	"context"
 	"testing"
+	"time"
 )
 
 func Test_store(t *testing.T) {
-	propc := make(chan<- string)
-	recvc := make(<-chan *string)
+	propc := make(chan string)
+	recvc := make(chan *string)
 	errc := make(chan error)
 
 	s := NewStore(propc, recvc, errc)
 	defer s.Stop()
 
+	donec := make(chan struct{})
+	go func() {
+		defer close(donec)
+		str := <-propc
+		recvc <- &str
+	}()
 	s.Propose(context.TODO(), "foo", "bar")
+	<-donec
+
+	time.Sleep(10 * time.Millisecond)
 
 	val, ok := s.Get("foo")
 	if !ok {
