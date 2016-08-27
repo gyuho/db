63bc4822af0a39f24b1d66f30ffc83431bde8b85
Gyu-Ho Lee <gyuhox@gmail.com>
2016-08-27 12:39:53 -0700

    fileutil: add 'ZeroToEnd'

---
+++ pkg/fileutil/zero.go
@@ -0,0 +1,54 @@
+package fileutil
+
+import "os"
+
+/*
+https://golang.org/pkg/os/#File.Seek
+
+Seek sets the offset for the next Read or Write on file to offset,
+interpreted according to whence:
+
+0 means relative to the origin of the file,
+1 means relative to the current offset, and
+2 means relative to the end.
+
+It returns the new offset and an error,
+if any. The behavior of Seek on a file opened with O_APPEND is not specified.
+
+func (f *File) Seek(offset int64, whence int) (ret int64, err error)
+
+const (
+        SEEK_SET int = 0 // seek relative to the origin of the file
+        SEEK_CUR int = 1 // seek relative to the current offset
+        SEEK_END int = 2 // seek relative to the end
+)
+*/
+
+// ZeroToEnd zeros a file from os.SEEK_CUR to its os.SEEK_END.
+// May temporarily shorten the length of the file.
+//
+// (etcd pkg.fileutil.ZeroToEnd)
+func ZeroToEnd(f *os.File) error {
+	// TODO: support FALLOC_FL_ZERO_RANGE
+	curOffset, err := f.Seek(0, os.SEEK_CUR)
+	if err != nil {
+		return err
+	}
+
+	endOffset, err := f.Seek(0, os.SEEK_END)
+	if err != nil {
+		return err
+	}
+
+	if err := f.Truncate(curOffset); err != nil {
+		return err
+	}
+
+	// make sure blocks remain allocated
+	if err := Preallocate(f, endOffset, true); err != nil {
+		return err
+	}
+
+	_, err = f.Seek(curOffset, os.SEEK_SET)
+	return err
+}
