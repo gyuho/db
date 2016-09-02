09db6e046b9fda78e8b37b728bb20ddc2690cbef
Gyu-Ho Lee <gyuhox@gmail.com>
2016-09-02 00:14:02 -0700

    pkg/ioutil: finish PageWriter tests

--- pkg/ioutil/pagewriter_test.go
+++ pkg/ioutil/pagewriter_test.go
@@ -24,7 +24,7 @@ func (cw *checkPageWriter) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
-// (etcd pkg.ioutil.TestPageWriterRandom
+// (etcd pkg.ioutil.TestPageWriterRandom)
 func Test_PageWriter_random(t *testing.T) {
 	defaultWatermarkN = 8 * 1024
 	pageChunkN := 128
@@ -33,6 +33,7 @@ func Test_PageWriter_random(t *testing.T) {
 
 	// randomly write empty bytes
 	data := make([]byte, 4*defaultWatermarkN)
+
 	n := 0
 	for i := 0; i < 4*1024; i++ {
 		c, err := pw.Write(data[:rand.Intn(len(data))])
@@ -53,6 +54,46 @@ func Test_PageWriter_random(t *testing.T) {
 	fmt.Printf("total writes(bytes) flushed: %d out of %d\n", cw.writeBytesN, n)
 }
 
+// (etcd pkg.ioutil.TestPageWriterPartialSlack)
 func Test_PageWriter_partial(t *testing.T) {
+	defaultWatermarkN = 1024
+	pageChunkN := 128
 
+	cw := &checkPageWriter{t: t, pageChunkN: 64}
+	pw := NewPageWriter(cw, pageChunkN)
+
+	// randomly write empty bytes
+	data := make([]byte, defaultWatermarkN)
+
+	// expecting only 1 write
+	if _, err := pw.Write(data[:64]); err != nil {
+		t.Fatal(err)
+	}
+	if err := pw.Flush(); err != nil {
+		t.Fatal(err)
+	}
+	if cw.writesN != 1 {
+		t.Fatalf("writesN expected 1, got %d", cw.writesN)
+	}
+
+	// nearly fill up the buffer, after flush
+	if _, err := pw.Write(data[:1022]); err != nil {
+		t.Fatal(err)
+	}
+	// overflow the buffer, but not enough to create
+	// another page alignment
+	if _, err := pw.Write(data[:8]); err != nil {
+		t.Fatal(err)
+	}
+	if cw.writesN != 1 {
+		t.Fatalf("writesN expected 1, got %d", cw.writesN)
+	}
+
+	// finish aligned write
+	if _, err := pw.Write(data[:pageChunkN]); err != nil {
+		t.Fatal(err)
+	}
+	if cw.writesN != 2 {
+		t.Fatalf("writesN expected 2, got %d", cw.writesN)
+	}
 }
