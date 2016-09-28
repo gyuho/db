b1ee9e980a82d480480d95628c03c5ee4fab0923
Gyu-Ho Lee <gyuhox@gmail.com>
2016-09-27 21:06:16 -0700

    pkg/fileutil: add comment

--- pkg/fileutil/zero.go
+++ pkg/fileutil/zero.go
@@ -21,10 +21,27 @@ if any. The behavior of Seek on a file opened with O_APPEND is not specified.
 func (f *File) Seek(offset int64, whence int) (ret int64, err error)
 
 const (
-        SEEK_SET int = 0 // seek relative to the origin of the file
-        SEEK_CUR int = 1 // seek relative to the current offset
-        SEEK_END int = 2 // seek relative to the end
+	SEEK_SET int = 0 // seek relative to the origin of the file  == io.SeekStart
+	SEEK_CUR int = 1 // seek relative to the current offset      == io.SeekCurrent
+	SEEK_END int = 2 // seek relative to the end                 == io.SeekEnd
 )
+
+
+// Seek repositions the offset of the open file from 'whence'
+f.Seek(offset, whence)
+
+// file's offset is set to 'offset' bytes
+f.Seek(offset, io.SeekStart)
+
+// file's offset is set to its current location + 'offset' bytes
+f.Seek(offset, io.SeekCurrent)
+
+// file's offset is set to its file size + 'offset' bytes
+// allows the file offset to be set beyond the end of the file
+// (but this does not change the size of the file)
+f.Seek(offset, io.SeekEnd)
+
+// f.Seek returns offset location in bytes from the beginning of the file
 */
 
 // ZeroToEnd zeros a file from io.SeekCurrent to its io.SeekEnd.
