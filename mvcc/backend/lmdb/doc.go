796a6245483760a661637df01255105c7f0b34de
Gyu-Ho Lee <gyuhox@gmail.com>
2016-11-19 21:24:12 -0800

    mvcc: add 'lmbdb' package description

---
+++ mvcc/backend/lmdb/doc.go
@@ -0,0 +1,3 @@
+// Package lmdb implements lightning memory-mapped key/value database.
+// Reference https://github.com/boltdb/bolt.
+package lmdb
--- storage/doc.go
+++
@@ -1,4 +0,0 @@
-// Package storage implements store.
-//
-// Reference implementation is https://github.com/boltdb/bolt.
-package storage
